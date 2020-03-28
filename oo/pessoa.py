c0f9b077f779a49e575da089d70703d622dda68b
Joca <jocascabral@icloud.com>
2020-03-28 17:11:03 -0300

    Criado atributo de class olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
 
     def __init__(self, *filhos, nome = None, idade = 43):
         self.nome = nome
@@ -21,6 +22,10 @@ if __name__ == '__main__':
     del joelson.filhos # excluindo um atributo -> também não é uma boa prática
     print(joelson.__dict__)  # dunder responsável por exibir os atributos de instância
     print(levi.__dict__)
+    Pessoa.olhos = 3
+    print(f'Valor de olhos {Pessoa.olhos}')
+    print(f'Olhos de Lévi {levi.olhos}')
+    print(id(Pessoa.olhos), id(levi.olhos), id(joelson.olhos))
 
 #    for filho in joelson.filhos:
 #        print(filho.nome)
